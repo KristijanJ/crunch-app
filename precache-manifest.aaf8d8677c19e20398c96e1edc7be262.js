acfa7e39a035362b362907750ce5f754c8f88c45
Kristijan Jovanovski <kristijan.j.mail@gmail.com>
2020-01-17 16:39:32 +0100

    Updates

---
+++ precache-manifest.aaf8d8677c19e20398c96e1edc7be262.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc2a91e73f479508eee24018d90cf617",
+    "url": "/crunch-app/index.html"
+  },
+  {
+    "revision": "ae20aaa7cb3076e23c6b",
+    "url": "/crunch-app/static/css/2.2eb3ca21.chunk.css"
+  },
+  {
+    "revision": "a6186673ff7ccfdf8751",
+    "url": "/crunch-app/static/css/main.9b7fd2cb.chunk.css"
+  },
+  {
+    "revision": "ae20aaa7cb3076e23c6b",
+    "url": "/crunch-app/static/js/2.1ec8eaac.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/crunch-app/static/js/2.1ec8eaac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6186673ff7ccfdf8751",
+    "url": "/crunch-app/static/js/main.8c0a1ec8.chunk.js"
+  },
+  {
+    "revision": "d5f23f920aec64f60f19",
+    "url": "/crunch-app/static/js/runtime-main.6450d6ac.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/crunch-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/crunch-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/crunch-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/crunch-app/static/media/slick.f97e3bbf.svg"
+  }
+]);
